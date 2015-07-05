bf791636d24f345ca0e913f0189bd7daebf940a8
Stefan Niculae <stefan1niculae@gmail.com>
2015-07-05 19:52:58 +0300

    virtualenv integration

--- Django Project/GodsAcre/settings.py
+++ Django Project/GodsAcre/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'CemeteryAdministration',
-    'django-forms-bootstrap',
+    'django_forms_bootstrap',
 )
 
 MIDDLEWARE_CLASSES = (
