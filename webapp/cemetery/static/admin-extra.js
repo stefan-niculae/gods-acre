b6e4086561399617e5e310f1cf5a2ec839ca06b8
Stefan Niculae <stefan1niculae@gmail.com>
2017-07-22 19:28:44 +0300

    Add tabbed front-end

--- webapp/cemetery/static/admin-extra.js
+++ webapp/cemetery/static/admin-extra.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log('a')
+    var tabs     = document.getElementsByClassName('changeform-tabs-item')
+    var contents = document.getElementsByClassName('module')
+
+    for (var selectedIndex = 0; selectedIndex < tabs.length; selectedIndex++)
+        tabs[selectedIndex].onclick = function (selectedIndex) {
+            return function() {
+                console.log('clicked', selectedIndex)
+
+                // remove 'selected' class from all tabs and contents
+                for (var i = 0; i < tabs.length; i++) {
+                    tabs    [i].classList.remove('selected')
+                    contents[i].classList.remove('selected')
+                }
+
+                // set 'selected' only desired tab and content
+                tabs    [selectedIndex].classList.add('selected')
+                contents[selectedIndex].classList.add('selected')
+            }
+        }(selectedIndex)
+
+    // TODO: toggle for sheet-wise fail, duplicate, add, selected
+    // TODO: line-through for row-wise selected
 })
